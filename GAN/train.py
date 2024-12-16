8cd97c3264ac44996c3a7b8c72a49f86f9420ae6
shayan <mmshokrii1996@gmail.com>
2024-12-16 13:59:01 -0500

    train updated

--- GAN/train.py
+++ GAN/train.py
@@ -19,15 +19,16 @@ def main():
     train_dataset, train_dataloader = loader.load()
 
     num_epochs = 50
-    lr = 0.0002
+    disc_lr = 1e-5
+    gen_lr = 1e-5
     model = GAN(latent_dim=100)
 
     model.train()
     generator = model.generator
     discriminator = model.discriminator
     discriminator_loss = nn.BCEWithLogitsLoss()
-    gen_optimizer = AdamW(generator.parameters(), lr=lr, betas=(0.5, 0.999))
-    disc_optimizer = AdamW(discriminator.parameters(), lr=lr, betas=(0.5, 0.999))
+    gen_optimizer = AdamW(generator.parameters(), lr=gen_lr, betas=(0.5, 0.999))
+    disc_optimizer = AdamW(discriminator.parameters(), lr=disc_lr, betas=(0.5, 0.999))
     
     for epoch in range(num_epochs):
         epoch_loss_disc = 0
@@ -42,31 +43,38 @@ def main():
             fake_images = generator(noise)
 
             # smooth the labels
-            real_labels = torch.full((batch_size, 1), 0.9).float()
+            smooth_real_labels = torch.full((batch_size, 1), 0.9).float()
+            real_labels = torch.ones(batch_size, 1).float()
             fake_labels = torch.zeros(batch_size, 1).float()
             
+            # Stack real and fake images
+            all_images = torch.cat((real_images, fake_images), dim=0)  
+            all_labels = torch.cat((smooth_real_labels, fake_labels), dim=0)
+            permutation = torch.randperm(all_images.size(0))  # Generate random permutation indices
+            shuffled_images = all_images[permutation]
+            shuffled_labels = all_labels[permutation]
             # -------------------------
             # Train Discriminator
             # -------------------------
             disc_optimizer.zero_grad()
-            disc_output_real = discriminator(real_images)
-            disc_output_fake = discriminator(fake_images.detach())
-            disc_loss_real = discriminator_loss(disc_output_real, real_labels)
-            disc_loss_fake = discriminator_loss(disc_output_fake, fake_labels)
-            disc_loss = (disc_loss_real + disc_loss_fake) * 0.5
+
+            disc_output = discriminator(shuffled_images)
+            disc_loss = discriminator_loss(disc_output, shuffled_labels)
             epoch_loss_disc += disc_loss
             disc_loss.backward()
             disc_optimizer.step()  
             # Calculate accuracy
-            real_pred = (disc_output_real > 0.5).float()
-            fake_pred = (disc_output_fake < 0.5).float()
-            correct_real = (real_pred == real_labels).sum().item()
-            correct_fake = (fake_pred == fake_labels).sum().item()
-            epoch_accuracy_disc += correct_real + correct_fake
+            predictions = (disc_output > 0.5).float()
+            binary_labels = (shuffled_labels > 0.5).float()
+            correct_predictions = (predictions == binary_labels).sum().item()
+
+            epoch_accuracy_disc += correct_predictions
             # -------------------------
             # Train Generator
             # -------------------------
             gen_optimizer.zero_grad()
+            noise = torch.randn(batch_size, 100).to(real_images.device)
+            fake_images = generator(noise)
             disc_output_fake = discriminator(fake_images)
             gen_loss = discriminator_loss(disc_output_fake, real_labels)
             gen_loss.backward()
